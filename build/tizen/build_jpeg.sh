910a6929bf6503369b4576f360e668e442efecbe
andyque <guanghui8827@126.com>
2014-10-30 16:40:02 +0800

    add scripts for building jpeg on Tizen

--- build/tizen/build_jpeg.sh
+++ build/tizen/build_jpeg.sh
@@ -1,5 +1,5 @@
 current_dir=`pwd`
-library_name=png
+library_name=jpeg
 ./build.sh -l $library_name
 top_dir=$current_dir/../..
 
@@ -8,7 +8,7 @@ mkdir -p $library_name/prebuilt/tizen/arm/
 mkdir -p $library_name/include/tizen/
 
 cp $top_dir/contrib/arm-linux-gnueabi-armv7a/lib/lib${library_name}.a $library_name/prebuilt/tizen/arm/
-cp $top_dir/contrib/arm-linux-gnueabi-armv7a/include/$library_name*.h  $library_name/include/tizen/
+cp $top_dir/contrib/arm-linux-gnueabi-armv7a/include/*.h  $library_name/include/tizen/
 
 echo "cleaning up"
 rm -rf $top_dir/contrib/arm-linux-gnueabi-armv7a/
