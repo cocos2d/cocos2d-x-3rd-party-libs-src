76146e1d9749ea13ef4a12250744c41275279078
andyque <guanghui8827@126.com>
2014-10-30 16:22:28 +0800

    add scripts to build png library on Tizen

--- build/tizen/build_png.sh
+++ build/tizen/build_png.sh
@@ -1,3 +1,10 @@
 current_dir=`pwd`
 ./build.sh -l png
 top_dir=$current_dir/../..
+
+cd $current_dir
+mkdir -p png/prebuilt/tizen/arm/
+mkdir -p png/include/tizen/
+
+cp $top_dir/contrib/arm-linux-gnueabi-armv7a/lib/libpng.a png/prebuilt/tizen/arm/
+cp $top_dir/contrib/arm-linux-gnueabi-armv7a/include/png*.h  png/include/tizen/
