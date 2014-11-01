7478157c7c46cdd00c729367ade861814b28b9c7
andyque <guanghui8827@126.com>
2014-11-01 22:46:46 +0800

    let png rely on our own zlib verion and add more compile flags to andrid

--- build/android/build.sh
+++ build/android/build.sh
@@ -115,6 +115,16 @@ export ANDROID_API
 # info "LD FLAGS SELECTED = '${LDFLAGS}'"
 
 export PATH="${toolchain_bin}:${cocos_root}/extras/tools/bin:$PATH"
+if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
+    export CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb -Os -g -DNDEBUG -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security  "
+elif [ "$ANDROID_ABI" = "armeabi" ]; then
+    export CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -march=armv5te -mtune=xscale -msoft-float -mthumb -Os -g -DNDEBUG -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
+else
+    export CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -fPIC -no-canonical-prefixes -O2 -g -DNDEBUG -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
+fi
+
+info "CFLAGS is ${CFLAGS}"
+
 #
 # build 3rd party libraries
 #
