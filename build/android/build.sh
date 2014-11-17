376848e508920f1a8debe286ab989d07f7601836
andyque <guanghui8827@126.com>
2014-11-17 16:21:32 +0800

    refactor build scripts

--- build/android/build.sh
+++ build/android/build.sh
@@ -1,199 +1,350 @@
 #!/bin/sh
-set -e
-set -x
 
-info()
+source `pwd`/config.ini
+#
+# A script to build static library for Android
+#
+
+build_arches=$cfg_default_build_arches
+build_mode=$cfg_default_build_mode
+build_library=$cfg_default_build_libraries
+build_api=$cfg_default_build_api
+build_gcc_version=$cfg_default_gcc_version
+
+function usage()
 {
-    local green="\033[1;32m"
-    local normal="\033[0m"
-    echo "[${green}build${normal}] $1"
+    echo "You should follow the instructions here to build static library for $cfg_platform_name"
+    echo ""
+    echo "./build_png.sh"
+    echo "\t[-h --help]  "
+    echo "\t--libs=[all | png,lua,tiff,jpeg,webp,zlib etc]"
+    echo "\t[--arch | -a]=[all | $cfg_help_arch_string etc]"
+    echo "\t[--mode | -m]=[release | debug]"
+    
+if [ $cfg_platform_name = "Android" ]; then
+    echo "\t[--api]=[18,19,20 etc, 19 is default.]"
+    echo "\t[--gcc]=[4.8+, default is 4.8]"
+fi
+    echo "\t[--list | -l]"
+    echo ""
+    echo "Sample:"
+    echo "\t.$cfg_help_sample_string"
+    echo ""
 }
 
-# TODO: You must set your ANDROID_NDK path in .bash_profile
-# source ~/.bash_profile
-ANDROID_ABI="armeabi-v7a"
-ANDROID_API="android-19"
-ANDROID_GCC_VERSION=4.8
-ANDROID_ARCH=arm
-BUILD_MODE=release
-
-# TODO: configure to compile specify 3rd party libraries
-OPTIONS=""
-IS_EXPORT_CFLAGS=yes
-
-usage()
+function list_all_supported_libraries()
 {
-cat << EOF
-usage: $0 [options]
-Build cocos2d-x 3rd party libraries for Android
-OPTIONS:
-   -h            Show some help
-   -q            Be quiet
-   -k <sdk>      Use the specified Android API level (default: $ANDROID_API)
-   -a <arch>     Use the specified arch (default: $ANDROID_ABI)
-   -n <version>  Use the gcc version(default: $ANDROID_GCC_VERSION)
-   -l <libname>  Use the specified library name
-   -e <export cflags> Whether to export cflags
-   -m <build mode>  Use release or debug mode
-EOF
+
+    # TODO: we need to update the supported libraries and version when we upgrade libraries
+    echo "Supported libraries and versions:"
+    echo "\t"
+    echo "\tcurl 7.26.0"
+    echo "\tfreetype 2.5.3"
+    echo "\tjpeg 9.0"
+    echo "\tlua 1.5.4"
+    echo "\tluajit 2.0.1"
+    echo "\topenssl 1.0.1.j"
+    echo "\tlibpng 1.6.2"
+    echo "\ttiff 4.0.3"
+    echo "\twebp 0.4.2"
+    echo "\twebsockets 1.3"
+    echo "\tzlib 1.2.8"
+    echo ""
 }
 
-spushd()
-{
-    pushd "$1" > /dev/null
-}
 
-spopd()
-{
-    popd > /dev/null
-}
-
-while getopts "hvk:a:l:e:n:m:" OPTION
-do
-     case $OPTION in
-         h)
-             usage
-             exit 1
-             ;;
-         q)
-             set +x
-             QUIET="yes"
-             ;;
-         a)
-             ANDROID_ABI=$OPTARG
-             ;;
-         k)
-             ANDROID_API=$OPTARG
-             ;;
-         n)
-             ANDROID_GCC_VERSION=$OPTARG
-             ;;
-         l)
-             OPTIONS=--enable-$OPTARG
-             ;;
-         e)
-             IS_EXPORT_CFLAGS=$OPTARG 
-             ;;
-         m)
-             BUILD_MODE=$OPTARG
-             ;;
-     esac
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        --help | -h)
+            usage
+            exit
+            ;;
+        --libs)
+            build_library=$VALUE
+            ;;
+        --arch | -a)
+            build_arches=$VALUE
+            ;;
+        --mode | -m)
+            build_mode=$VALUE
+            ;;
+        --list | -l)
+            list_all_supported_libraries
+            exit
+            ;;
+        --api)
+            build_api=$VALUE
+            ;;
+        --gcc)
+            build_gcc_version=$VALUE
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
 done
 
-if test -z "$OPTIONS"
+if test -z "$build_arches"
 then
-    echo "You must specify a OPTIONS parameter."
+    echo "You must speficy a valid arch option"
     usage
     exit 1
 fi
 
-if [ "${ANDROID_ABI}" != "x86" ] && [ "${ANDROID_ABI}" != "armeabi-v7a" ] && [ "${ANDROID_ABI}" != "armeabi" ]; then
-    echo "You must specify the right Android Arch within {armeabi, armeabi-v7a, x86}"
+if test -z "$build_library"
+then
+    echo "You must specify a valid library option"
+    usage
     exit 1
 fi
 
-if [ $BUILD_MODE = "release" ]; then
-    OPTIM="-O3 -DNDEBUG"
+if test -z "$build_mode"
+then
+    echo "You must specify a valid mode option"
+    usage
+    exit 1
 fi
 
-if [ $BUILD_MODE = "debug" ]; then
-    OPTIM="-O0 -g -DDEBUG"
+echo "build api is $build_api."
+if [[ ! $build_api =~ ^[0-9]+$ ]]; then
+    echo "Android API should be integers!"
+    usage
+    exit 1
 fi
 
-# FIXME: we need a way to determine the toolchina address automatically
-toolchain_bin=
+if [[ ! $build_gcc_version =~ ^[0-9]\.[0-9]+$ ]]; then
+    echo "Invalid gcc version number! Gcc version should be numerical numbers."
+    usage
+    exit 1
+fi 
 
-if [ "${ANDROID_ABI}" = "x86" ]; then
-    TARGET="i686-linux-android"
-    toolchain_bin=${ANDROID_NDK}/toolchains/x86-${ANDROID_GCC_VERSION}/prebuilt/darwin-x86_64/bin
-    ANDROID_ARCH=x86
+function contains() {
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $#;i++)) {
+            if [ "${!i}" == "${value}" ]; then
+                echo "y"
+                return 0
+            fi
+        }
+        echo "n"
+        return 1
+}
+
+all_arches=(${cfg_all_supported_arches[@]})
+all_libraries=(${cfg_all_supported_libraries[@]})
+
+if [ $build_arches = $cfg_default_build_arches ]; then
+    build_arches=(${cfg_default_arches_all[@]})
 else
-    TARGET="arm-linux-androideabi"
-    toolchain_bin=${ANDROID_NDK}/toolchains/${TARGET}-${ANDROID_GCC_VERSION}/prebuilt/darwin-x86_64/bin
+    build_arches=(${build_arches//,/ })
 fi
 
-# check whether gcc version is exists
-if [ ! -d ${ANDROID_NDK}/toolchains/x86-${ANDROID_GCC_VERSION} ] && [ ! -d ${ANDROID_NDK}/toolchains/${TARGET}-${ANDROID_GCC_VERSION} ] ;then
-    echo "Invalid GCC version!"
+if [ $build_library = $cfg_default_build_libraries ]; then
+    build_library=(${cfg_default_libraries_all[@]})
+else
+    build_library=(${build_library//,/ })
+fi
+
+#check invalid arch type
+function check_invalid_arch_type()
+{
+    for arch in ${build_arches[@]}
+    do
+        echo "checking ${arch} is in ${all_arches[@]}"
+        if [ $(contains "${all_arches[@]}" $arch) == "n" ]; then
+            echo "Invalid arch! Only ${all_arches[@]} is acceptable."
+            exit 1
+        fi
+    done
+}
+
+check_invalid_arch_type
+
+#check invalid library name
+function check_invalid_library_name()
+{
+    for lib in "${build_library[@]}"
+    do
+        echo "checking ${lib} is in ${all_libraries[@]}"
+        if [ $(contains "${all_libraries[@]}" $lib) == "n" ]; then
+            echo "Invalid library names! Only ${all_libraries[@]} is acceptable!"
+            exit 1
+        fi
+    done
+}
+
+check_invalid_library_name
+
+#check invalid build mode, only debug and release is acceptable
+if [ $build_mode != "release" ] && [ $build_mode != "debug" ]; then
+    echo "invalid build mode, only: debug and release is acceptabl"
+    usage
     exit
 fi
 
-shift $(($OPTIND - 1))
-if [ "x$1" != "x" ]; then
-    usage
-    exit 1
-fi
-#
-# Various initialization
-#
-out="/dev/stdout"
-if [ "$QUIET" = "yes" ]; then
-    out="/dev/null"
-fi
-
-info "Building 3rd party libraries for the Android"
-cocos_root=`pwd`/../..
-
-export ANDROID_ABI
-export ANDROID_API
-
-export PATH="${toolchain_bin}:${cocos_root}/extras/tools/bin:$PATH"
-
-if [ $IS_EXPORT_CFLAGS = "yes" ];then
-
-if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
-export LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
-fi
-info "LD FLAGS SELECTED = '${LDFLAGS}'"
-
-if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
-    export CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security  "
-elif [ "$ANDROID_ABI" = "armeabi" ]; then
-    export CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -march=armv5te -mtune=xscale -msoft-float -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
-else
-    export CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -fPIC -no-canonical-prefixes $OPTIM -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
-fi
-
-info "CFLAGS is ${CFLAGS}"
-else
-
-#don't export cflags
-if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
-LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
-fi
-info "LD FLAGS SELECTED = '${LDFLAGS}'"
-
-if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
-CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security  "
-elif [ "$ANDROID_ABI" = "armeabi" ]; then
-CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -march=armv5te -mtune=xscale -msoft-float -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
-else
-CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -fPIC -no-canonical-prefixes $OPTIM -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
-fi
-
-info "CFLAGS is ${CFLAGS}"
-fi
 
 
-#
-# build 3rd party libraries
-#
-info "Building static libraries"
-spushd "${cocos_root}/contrib"
-mkdir -p "Android-${ANDROID_ABI}" && cd "Android-${ANDROID_ABI}"
+function create_fat_library()
+{
+    library_name=$1
+    #strip & create fat library
+    LIPO="xcrun -sdk iphoneos lipo"
+    STRIP="xcrun -sdk iphoneos strip"
+
+    if [ -f $library_name/prebuilt/lib$library_name.a ]; then
+        echo "removing old fat library..."
+        rm $library_name/prebuilt/lib$library_name.a
+    fi
+
+    all_static_libs=$(find $library_name/prebuilt -type f -name "lib$library_name-*.a")
+
+    echo "create fat library lib$library_name for $all_static_libs"
+    $LIPO -create  $all_static_libs \
+          -output $library_name/prebuilt/lib$library_name.a
+
+    rm $all_static_libs
+
+    # remove debugging info don't strip
+    # $STRIP -S $library_name/prebuilt/lib$library_name.a
+    $LIPO -info $library_name/prebuilt/lib$library_name.a
+}
 
 
-../bootstrap ${OPTIONS} \
-             --host=${TARGET} \
-             --prefix=${cocos_root}/contrib/install-android/${ANDROID_ABI}> $out
+# build all the libraries for different arches
+for lib in "${build_library[@]}"
+do
+    library_name=$lib
+    archive_name=$lib
+    current_dir=`pwd`
+    top_dir=$current_dir/../..
 
-echo "ANDROID_ARCH := ${CFLAGS}" >> config.mak
-echo "BUILD_MODE := ${BUILD_MODE}" >> config.mak
-echo "OPTIM := ${OPTIM}" >> config.mak
+    build_script_name=$cfg_build_script_name
+    is_export_cflags=yes
+    if [ $lib = "luajit" ]; then
+        is_export_cflags=no
+    fi
 
-#
-# make
-#
-make fetch
-make list
-make
+    if [ $lib = "zlib" ]; then
+        archive_name=z
+    fi
+
+    if [ $lib = "freetype2" ]; then
+        archive_name=freetype
+    fi
+
+    mkdir -p $archive_name/prebuilt/
+    mkdir -p $archive_name/include/
+
+    for arch in "${build_arches[@]}"
+    do
+        #skip certain arch libraries
+        if [ $lib = "luajit" ] && [ $arch = "arm64" ]; then
+            continue
+        fi
+
+        if [ $lib = "luajit" ] && [ $arch = "x86_64" ]; then
+            continue
+        fi
+
+
+        install_library_path=$cfg_library_install_prefix
+        build_library_path=$cfg_library_build_folder
+
+        echo "build $arch for $lib"
+        if [ $cfg_platform_name = "Android" ];then
+            $top_dir/contrib/$build_script_name  -a $arch -l $library_name -m $build_mode -e $is_export_cflags -k $build_api -n $build_gcc_version
+        fi
+
+        cp $top_dir/contrib/$install_library_path/$arch/lib/lib$archive_name.a $archive_name/prebuilt/lib$archive_name-$arch.a
+        # FIXME: some archive names have some postfix in it.
+        cp $top_dir/contrib/$install_library_path/$arch/lib/lib$archive_name*.a $archive_name/prebuilt/lib$archive_name-$arch.a
+
+
+        if [ $lib = "curl" ]; then
+            mkdir -p ssl/prebuilt/
+            cp $top_dir/contrib/$install_library_path/$arch/lib/libssl.a ssl/prebuilt/libssl-$arch.a
+
+            mkdir -p crypto/prebuilt/
+            cp $top_dir/contrib/$install_library_path/$arch/lib/libcrypto.a crypto/prebuilt/libcrypto-$arch.a
+
+            echo "copying libz..."
+            mkdir -p z/prebuilt/
+            cp $top_dir/contrib/$install_library_path/$arch/lib/libz.a z/prebuilt/libz-$arch.a
+        fi
+
+        if [ $lib = "png" ] || [ $lib = "freetype2" ] || [ $lib = "websockets" ];  then
+            echo "copying libz..."
+            mkdir -p z/prebuilt/
+            cp $top_dir/contrib/$install_library_path/$arch/lib/libz.a z/prebuilt/libz-$arch.a
+        fi
+
+        echo "Copying needed heder files"
+        if [ $lib = "png" ]; then
+            cp  $top_dir/contrib/$install_library_path/$arch/include/png*.h  $archive_name/include/
+        fi
+
+        if [ $lib = "luajit" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/luajit-2.0/  $archive_name/include/
+        fi
+
+        if [ $lib = "lua" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/l*.h*  $archive_name/include/
+        fi
+
+        if [ $lib = "curl" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/curl/  $archive_name/include/
+        fi
+
+        if [ $lib = "freetype2" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/freetype2  $archive_name/include
+        fi
+
+        if [ $lib = "jpeg" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/j*.h  $archive_name/include/
+        fi
+
+        if [ $lib = "tiff" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/tif*.h  $archive_name/include/
+        fi
+
+        if [ $lib = "webp" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/webp/  $archive_name/include/
+        fi
+
+        if [ $lib = "websockets" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/libwebsockets.h  $archive_name/include/
+        fi
+
+        if [ $lib = "zlib" ]; then
+            cp -r $top_dir/contrib/$install_library_path/$arch/include/z*.h  $archive_name/include/
+        fi
+
+        # TODO: add more header files decides here
+
+        echo "cleaning up"
+        # FIXME: uncomment it for debug purpose
+        # rm -rf $top_dir/contrib/$install_library_path
+        # rm -rf $top_dir/contrib/$build_library_path-$arch
+    done
+
+    if [ $cfg_platform_name = "iOS" ] || [ $cfg_platform_name = "Mac" ]; then
+        
+        create_fat_library $archive_name
+
+        if [ $lib = "curl" ]; then
+            create_fat_library ssl
+            create_fat_library crypto
+        fi
+
+        if [ $lib = "png" ] || [ $lib = "curl" ] || [ $lib = "freetype2" ] || [ $lib = "websockets" ]; then
+            create_fat_library z
+        fi
+    fi
+
+done
---
+++ contrib/build_android.sh
@@ -0,0 +1,199 @@
+#!/bin/sh
+set -e
+set -x
+
+info()
+{
+    local green="\033[1;32m"
+    local normal="\033[0m"
+    echo "[${green}build${normal}] $1"
+}
+
+# TODO: You must set your ANDROID_NDK path in .bash_profile
+# source ~/.bash_profile
+ANDROID_ABI="armeabi-v7a"
+ANDROID_API=19
+ANDROID_GCC_VERSION=4.8
+ANDROID_ARCH=arm
+BUILD_MODE=release
+
+# TODO: configure to compile specify 3rd party libraries
+OPTIONS=""
+IS_EXPORT_CFLAGS=yes
+
+usage()
+{
+cat << EOF
+usage: $0 [options]
+Build cocos2d-x 3rd party libraries for Android
+OPTIONS:
+   -h            Show some help
+   -q            Be quiet
+   -k <sdk>      Use the specified Android API level (default: $ANDROID_API)
+   -a <arch>     Use the specified arch (default: $ANDROID_ABI)
+   -n <version>  Use the gcc version(default: $ANDROID_GCC_VERSION)
+   -l <libname>  Use the specified library name
+   -e <export cflags> Whether to export cflags
+   -m <build mode>  Use release or debug mode
+EOF
+}
+
+spushd()
+{
+    pushd "$1" > /dev/null
+}
+
+spopd()
+{
+    popd > /dev/null
+}
+
+while getopts "hvk:a:l:e:n:m:" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         q)
+             set +x
+             QUIET="yes"
+             ;;
+         a)
+             ANDROID_ABI=$OPTARG
+             ;;
+         k)
+             ANDROID_API=android-$OPTARG
+             ;;
+         n)
+             ANDROID_GCC_VERSION=$OPTARG
+             ;;
+         l)
+             OPTIONS=--enable-$OPTARG
+             ;;
+         e)
+             IS_EXPORT_CFLAGS=$OPTARG 
+             ;;
+         m)
+             BUILD_MODE=$OPTARG
+             ;;
+     esac
+done
+
+if test -z "$OPTIONS"
+then
+    echo "You must specify a OPTIONS parameter."
+    usage
+    exit 1
+fi
+
+if [ "${ANDROID_ABI}" != "x86" ] && [ "${ANDROID_ABI}" != "armeabi-v7a" ] && [ "${ANDROID_ABI}" != "armeabi" ]; then
+    echo "You must specify the right Android Arch within {armeabi, armeabi-v7a, x86}"
+    exit 1
+fi
+
+if [ $BUILD_MODE = "release" ]; then
+    OPTIM="-O3 -DNDEBUG"
+fi
+
+if [ $BUILD_MODE = "debug" ]; then
+    OPTIM="-O0 -g -DDEBUG"
+fi
+
+# FIXME: we need a way to determine the toolchina address automatically
+toolchain_bin=
+
+if [ "${ANDROID_ABI}" = "x86" ]; then
+    TARGET="i686-linux-android"
+    toolchain_bin=${ANDROID_NDK}/toolchains/x86-${ANDROID_GCC_VERSION}/prebuilt/darwin-x86_64/bin
+    ANDROID_ARCH=x86
+else
+    TARGET="arm-linux-androideabi"
+    toolchain_bin=${ANDROID_NDK}/toolchains/${TARGET}-${ANDROID_GCC_VERSION}/prebuilt/darwin-x86_64/bin
+fi
+
+# check whether gcc version is exists
+if [ ! -d ${ANDROID_NDK}/toolchains/x86-${ANDROID_GCC_VERSION} ] && [ ! -d ${ANDROID_NDK}/toolchains/${TARGET}-${ANDROID_GCC_VERSION} ] ;then
+    echo "Invalid GCC version!"
+    exit
+fi
+
+shift $(($OPTIND - 1))
+if [ "x$1" != "x" ]; then
+    usage
+    exit 1
+fi
+#
+# Various initialization
+#
+out="/dev/stdout"
+if [ "$QUIET" = "yes" ]; then
+    out="/dev/null"
+fi
+
+info "Building 3rd party libraries for the Android"
+script_root=`pwd`/../..
+
+export ANDROID_ABI
+export ANDROID_API
+
+export PATH="${toolchain_bin}:${script_root}/extras/tools/bin:$PATH"
+
+if [ $IS_EXPORT_CFLAGS = "yes" ];then
+
+if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
+export LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
+fi
+info "LD FLAGS SELECTED = '${LDFLAGS}'"
+
+if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
+    export CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security  "
+elif [ "$ANDROID_ABI" = "armeabi" ]; then
+    export CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -march=armv5te -mtune=xscale -msoft-float -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
+else
+    export CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -fPIC -no-canonical-prefixes $OPTIM -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
+fi
+
+info "CFLAGS is ${CFLAGS}"
+else
+
+#don't export cflags
+if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
+LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
+fi
+info "LD FLAGS SELECTED = '${LDFLAGS}'"
+
+if [ "$ANDROID_ABI" = "armeabi-v7a" ]; then
+CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security  "
+elif [ "$ANDROID_ABI" = "armeabi" ]; then
+CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -no-canonical-prefixes  -march=armv5te -mtune=xscale -msoft-float -mthumb $OPTIM -fomit-frame-pointer -fno-strict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
+else
+CFLAGS="-ffunction-sections -funwind-tables -fstack-protector -fPIC -no-canonical-prefixes $OPTIM -fomit-frame-pointer -fstrict-aliasing -DANDROID  -Wa,--noexecstack -Wformat -Werror=format-security"
+fi
+
+info "CFLAGS is ${CFLAGS}"
+fi
+
+
+#
+# build 3rd party libraries
+#
+info "Building static libraries"
+spushd "${script_root}/contrib"
+mkdir -p "Android-${ANDROID_ABI}" && cd "Android-${ANDROID_ABI}"
+
+
+../bootstrap ${OPTIONS} \
+             --host=${TARGET} \
+             --prefix=${script_root}/contrib/install-android/${ANDROID_ABI}> $out
+
+echo "ANDROID_ARCH := ${CFLAGS}" >> config.mak
+echo "BUILD_MODE := ${BUILD_MODE}" >> config.mak
+echo "OPTIM := ${OPTIM}" >> config.mak
+
+#
+# make
+#
+make fetch
+make list
+make
