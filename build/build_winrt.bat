c628c5b8b288a3209ea85c2b4421d7f255f858b6
Dale Stammen <dalestam@microsoft.com>
2016-04-13 14:06:38 -0700

    updated winrt build all scripts

--- build/build_winrt.bat
+++ build/build_winrt.bat
@@ -9,19 +9,45 @@ if exist %INSTALL_DIR% (
 
 mkdir %INSTALL_DIR%
 
+rem zlib and openssl must be built first!
 call:winrt_build zlib
+call:winrt_install zlib
+
+call:winrt_build openssl
+rem don't install openssl
+
 call:winrt_build angle
+call:winrt_install angle
+
 call:winrt_build chipmunk
+call:winrt_install chipmunk
+
+call:winrt_build curl
+call:winrt_install curl
+
 call:winrt_build freetype
+call:winrt_install freetype
+
 call:winrt_build ogg
+call:winrt_install ogg
+
 call:winrt_build sqlite
+call:winrt_install sqlite
+
 call:winrt_build websockets
+call:winrt_install websockets
+
 goto:eof
 
 :winrt_build  
 pushd ..\contrib\src\%~1\winrt
 echo Building %~1...
-call cmd /c "dobuild.bat"
+start /wait cmd /c "dobuild.bat"
+popd
+goto:eof
+
+:winrt_install  
+pushd ..\contrib\src\%~1\winrt
 echo Installing %~1...
 xcopy install %INSTALL_DIR% /iycqs
 popd
---
+++ build/dobuild_winrt.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start /wait cmd /c "build_winrt.bat"
+
