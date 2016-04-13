d62084591699df3020128dec4568e37a19cc5512
Dale Stammen <dalestam@microsoft.com>
2016-04-13 14:20:16 -0700

    added win10 build scripts

--- contrib/src/ogg/winrt/build.bat
+++ contrib/src/ogg/winrt/build.bat
@@ -28,8 +28,10 @@ if not exist %VORBIS.TAR.GZ% (
 	curl -O -L %VORBIS_URL%
 )
 
-tar -xzvf %OGG.TAR.GZ%
-tar -xzvf %VORBIS.TAR.GZ%
+echo Decompressing %OGG.TAR.GZ%
+tar -xzf %OGG.TAR.GZ%
+echo Decompressing %VORBIS.TAR.GZ%
+tar -xzf %VORBIS.TAR.GZ%
 
 mv libogg-%OGG_VERSION% libogg
 mv libvorbis-%VORBIS_VERSION% libvorbis
@@ -38,12 +40,15 @@ echo Patching libogg and libvorbis...
 xcopy ..\patch\libogg\win32 libogg\win32 /iycqs
 xcopy ..\patch\libvorbis\win32 libvorbis\win32 /iycqs
 
-call "%VS120COMNTOOLS%vsvars32.bat"
+call "%VS140COMNTOOLS%vsvars32.bat"
 
 set SOLUTION=libogg\win32\VS2013\libogg-win8.1-universal.sln
 
 echo Building Ogg Windows 8.1 Release/Win32...
 msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="Win32" /m
+
+rem echo Building Ogg Windows 8.1 Release/x64...
+rem msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="x64" /m
 
 echo Building Ogg Windows 8.1 Release/ARM...
 msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="ARM" /m
@@ -53,7 +58,32 @@ set SOLUTION=libvorbis\win32\VS2013\libvorbis-win8.1-universal.sln
 echo Building Ogg Windows 8.1 Release/Win32...
 msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="Win32" /m
 
+rem echo Building Ogg Windows 8.1 Release/x64...
+rem msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="x64" /m
+
 echo Building Ogg Windows 8.1 Release/ARM...
+msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="ARM" /m
+
+set SOLUTION=libogg\win32\VS2015\libogg-win10-universal.sln
+
+echo Building Ogg Windows 10.0 Release/x86...
+msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="x86" /m
+
+rem echo Building Ogg Windows 10.0 Release/x64...
+rem msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="x64" /m
+
+echo Building Ogg Windows 10.0 Release/ARM...
+msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="ARM" /m
+
+set SOLUTION=libvorbis\win32\VS2015\libvorbis-win10-universal.sln
+
+echo Building Ogg Windows 10.0 Release/x86...
+msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="x86" /m
+
+rem echo Building Ogg Windows 10.0 Release/x64...
+rem msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="x64" /m
+
+echo Building Ogg Windows 10.0 Release/ARM...
 msbuild %SOLUTION% /p:Configuration="Release" /p:Platform="ARM" /m
 
 popd
@@ -65,11 +95,15 @@ xcopy "temp\libogg\include\ogg" "install\winrt_8.1-specific\OggDecoder\include\o
 xcopy "temp\libvorbis\include\vorbis" "install\winrt_8.1-specific\OggDecoder\include\vorbis" /iycqs
 xcopy "temp\libogg\include\ogg" "install\wp_8.1-specific\OggDecoder\include\ogg" /iycqs
 xcopy "temp\libvorbis\include\vorbis" "install\wp_8.1-specific\OggDecoder\include\vorbis" /iycqs
+xcopy "temp\libogg\include\ogg" "install\win10-specific\OggDecoder\include\ogg" /iycqs
+xcopy "temp\libvorbis\include\vorbis" "install\win10-specific\OggDecoder\include\vorbis" /iycqs
 
 rem copy libs and dlls
-call:CopyOggFiles Windows win32 winrt_8.1
-call:CopyOggFiles Windows arm winrt_8.1
-call:CopyOggFiles WindowsPhone win32 wp_8.1
+call:CopyOggWin10Files win32
+call:CopyOggWin10Files arm
+call:CopyOggFiles Windows win32 winrt_8.1 
+call:CopyOggFiles Windows arm winrt_8.1 
+call:CopyOggFiles WindowsPhone win32 wp_8.1 
 call:CopyOggFiles WindowsPhone arm wp_8.1
 
 echo Ogg build complete.
@@ -97,4 +131,28 @@ echo INDIR=%INDIR%
 xcopy "%INDIR%\libvorbisfile.lib" "%OUTDIR%\*" /iycq
 xcopy "%INDIR%\libvorbisfile.dll" "%OUTDIR%\*" /iycq
 goto:eof
+
+:CopyOggWin10Files   
+set INDIR=temp\libogg\win32\VS2015\%~1\Release\libogg-win10-universal
+set OUTDIR=install\win10-specific\OggDecoder\prebuilt\%~1
+
+echo INDIR=%INDIR%
+echo OUTDIR=%OUTDIR%
+xcopy "%INDIR%\libogg.lib" "%OUTDIR%\*" /iycq
+xcopy "%INDIR%\libogg.dll" "%OUTDIR%\*" /iycq
+
+set INDIR=temp\libvorbis\win32\VS2015\%~1\Release\libvorbis-win10-universal
+echo INDIR=%INDIR%
+
+xcopy "%INDIR%\libvorbis.lib" "%OUTDIR%\*" /iycq
+xcopy "%INDIR%\libvorbis.dll" "%OUTDIR%\*" /iycq
+
+set INDIR=temp\libvorbis\win32\VS2015\%~1\Release\libvorbisfile-win10-universal
+echo INDIR=%INDIR%
+
+xcopy "%INDIR%\libvorbisfile.lib" "%OUTDIR%\*" /iycq
+xcopy "%INDIR%\libvorbisfile.dll" "%OUTDIR%\*" /iycq
+
+goto:eof
+
 
