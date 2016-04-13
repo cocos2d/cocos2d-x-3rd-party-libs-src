c738eac02dab0dece6dedd61c1003a41e8046b29
Dale Stammen <dalestam@microsoft.com>
2016-04-13 14:22:17 -0700

    updated to version 3120100

--- contrib/src/sqlite/winrt/build.bat
+++ contrib/src/sqlite/winrt/build.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set VERSION=3081002
-set PHONE_URL=https://www.sqlite.org/2015/sqlite-wp81-winrt-%VERSION%.vsix
-set WINRT_URL=https://www.sqlite.org/2015/sqlite-winrt81-%VERSION%.vsix
+set VERSION=3120100
+set PHONE_URL=https://www.sqlite.org/2016/sqlite-wp81-winrt-%VERSION%.vsix
+set WINRT_URL=https://www.sqlite.org/2016/sqlite-winrt81-%VERSION%.vsix
+set WIN10_URL=https://www.sqlite.org/2016/sqlite-uwp-%VERSION%.vsix
+
 
 if exist temp (
 	rm -rf temp
@@ -23,9 +25,14 @@ pushd temp
 	if not exist sqlite-wp81-winrt-%VERSION%.vsix (
 		curl -O -L %PHONE_URL%
 	)
+	
+	if not exist sqlite-uwp-%VERSION%.vsix (
+		curl -O -L %WIN10_URL%
+	)
 
 	unzip sqlite-winrt81-%VERSION%.vsix -d winrt_8.1
 	unzip sqlite-wp81-winrt-%VERSION%.vsix	-d wp_8.1
+	unzip sqlite-uwp-%VERSION%.vsix	-d win10
 popd
 
 echo Installing sqlite...
@@ -47,6 +54,16 @@ xcopy "%INDIR%\DesignTime\Retail\x86\sqlite3.lib" "%OUTDIR%\*" /iycq
 xcopy "%INDIR%\Redist\Retail\x86\sqlite3.dll" "%OUTDIR%\*" /iycq
 
 set OUTDIR=install\sqlite3\libraries\winrt_8.1\arm
+xcopy "%INDIR%\DesignTime\Retail\ARM\sqlite3.lib" "%OUTDIR%\*" /iycq
+xcopy "%INDIR%\Redist\Retail\ARM\sqlite3.dll" "%OUTDIR%\*" /iycq
+
+set INDIR=temp\win10\
+
+set OUTDIR=install\sqlite3\libraries\win10\win32
+xcopy "%INDIR%\DesignTime\Retail\x86\sqlite3.lib" "%OUTDIR%\*" /iycq
+xcopy "%INDIR%\Redist\Retail\x86\sqlite3.dll" "%OUTDIR%\*" /iycq
+
+set OUTDIR=install\sqlite3\libraries\win10\arm
 xcopy "%INDIR%\DesignTime\Retail\ARM\sqlite3.lib" "%OUTDIR%\*" /iycq
 xcopy "%INDIR%\Redist\Retail\ARM\sqlite3.dll" "%OUTDIR%\*" /iycq
 
